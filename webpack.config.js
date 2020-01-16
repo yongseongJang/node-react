e0da56bc6442522c358e995272177d2dab18b32a
yongseongJang <kgucs13@gmail.com>
2020-01-16 16:29:58 +0900

    config webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/public/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist/client'),
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+
+        ]
+    },
+    devServer: {
+        host: '192.168.171.135',
+        port: 9000,
+        open: true,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './views/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin()
+    ]
+}
