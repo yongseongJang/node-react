62a352c55b7e9588403f95620ba21d6eaf968b9a
yongseongJang <kgucs13@gmail.com>
2020-02-14 16:14:11 +0900

    feat(log): add morgan, winston logger

--- src/server/loaders/express.js
+++ src/server/loaders/express.js
@@ -3,15 +3,35 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { handleError } = require('../utils/error');
 
+const morgan = require('morgan');
+const logger = require('../utils/logger');
+
 exports.expressLoader = app => {
   app.use(express.static(path.join(__dirname, './../../dist')));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(
+    morgan('common', {
+      skip: () => {
+        return res.statusCode >= 400;
+      },
+      stream: process.stdout,
+    }),
+  );
+
+  app.use(
+    morgan('common', {
+      skip: () => {
+        return res.statusCode < 400;
+      },
+      stream: process.stderr,
+    }),
+  );
 
   require('../routes')(app);
 
   app.use((err, req, res, next) => {
-    console.error(err);
+    logger.error(err);
     next(err);
   });
 
