43eeb665c354856658536c03f5dfa187e5e521bf
yongseongJang <kgucs13@gmail.com>
2020-02-19 12:20:40 +0900

    test(authentication): jwt auth 테스트 추가

--- test/routes/middlewares/authentication.test.js
+++ test/routes/middlewares/authentication.test.js
@@ -1,8 +1,10 @@
 const async = require('async');
 const authentication = require('../../../src/server/routes/middlewares/authentication');
 const { Errorhandler } = require('../../../src/server/utils/error');
+const mongod = require('../../mongod');
+const UserService = require('../../../src/server/services/user.service');
 
-describe('authentication', () => {
+describe('authentication unit test', () => {
   it('should throw error if authHeader is not exist', () => {
     const req = { headers: {} };
     const res = {};
@@ -30,5 +32,39 @@ describe('authentication', () => {
     expect(next).toHaveBeenCalledWith(
       new Errorhandler(401, 'ValidationError', 'Invalid token'),
     );
+  });
+});
+
+describe('authentication integration test', () => {
+  beforeAll(async () => {
+    await mongod.connect();
+  });
+
+  afterEach(async () => {
+    await mongod.clear();
+  });
+
+  afterAll(async () => {
+    await mongod.close();
+  });
+  it('should have property user in req if token was validated', async () => {
+    const userInfo = {
+      email: 'test@gmail.com',
+      pw: '123',
+      userName: 'google',
+    };
+
+    await UserService.registerUserInfo(userInfo);
+
+    const token = await UserService.login(userInfo.email, userInfo.pw);
+    const headers = { authorization: 'Bearer ' + token };
+    const req = { headers };
+    const res = {};
+    const next = jest.fn();
+
+    authentication(req, res, next);
+
+    expect(req.user.email).toBe(userInfo.email);
+    expect(next).toHaveBeenCalledWith();
   });
 });
